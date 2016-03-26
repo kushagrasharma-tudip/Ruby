c5fc984aa94019d48ba03b3ac358663e28251113
kushagrasharma-tudip <kushagra.sharma@tudip.nl>
2016-03-26 14:12:22 +0530

    chaned

--- h.rb
+++ h.rb
@@ -6,3 +6,5 @@ puts %{Ruby is fun.}  "equivalent to" "Ruby is fun."
 puts %Q{ Ruby is fun. } "equivalent to" " Ruby is fun. "
 puts %q[Ruby is fun.]  "equivalent to a single-quoted string"
 
+#changes
+
